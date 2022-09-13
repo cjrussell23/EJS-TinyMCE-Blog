d0d82b0e530515186927392272d07e48451b2388
cjrussell23 <cj.russell2396@gmail.com>
2022-09-13 16:55:21 -0600

    Swipe navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,62 @@
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    if (scroll > 0){
+    if (scroll > 0) {
         const nav = document.getElementById("main-nav");
         nav.classList.add("scrolled");
     }
-    else{
+    else {
         const nav = document.getElementById("main-nav");
         nav.classList.remove("scrolled");
     }
 });
 console.log("Scroll event listener added");
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            /* right swipe */
+            console.log("right swipe");
+            document.getElementById("nav-toggle").click();
+        } else {
+            /* left swipe */
+            console.log("left swipe");
+            document.getElementById("nav-toggle").click();
+        }
+    } else {
+        if (yDiff > 0) {
+            /* down swipe */
+        } else {
+            /* up swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
