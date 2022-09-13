a0290a48fee54560762dd585ee6ae7580f11c428
cjrussell23 <cj.russell2396@gmail.com>
2022-09-13 15:21:08 -0600

    Navbar tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,10 @@ window.addEventListener("scroll", (event) => {
     if (scroll > 0){
         const nav = document.getElementById("main-nav");
         nav.classList.add("scrolled");
-        nav.classList.remove("not-scrolled");
     }
     else{
         const nav = document.getElementById("main-nav");
         nav.classList.remove("scrolled");
-        nav.classList.add("not-scrolled");
     }
 });
-
-function toggleHeader(){
-    const headerText = document.getElementById("header-text");
-    const spacer = document.getElementById("spacer");
-    headerText.classList.toggle("hidden");
-    spacer.classList.toggle("hidden");
-}
+console.log("Scroll event listener added");
